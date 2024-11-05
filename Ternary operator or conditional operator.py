3d875538cdfaeaf5af5576824c17d4520042a2ec
umesh Bhusal <anishbhusal30@gmail.com>
2024-11-05 17:35:43 +0545

    learning python

--- Ternary operator or conditional operator.py
+++ Ternary operator or conditional operator.py
@@ -1,4 +1,5 @@
 #Lesson 6
+from itertools import chain
 
 # syntax:
 # x = (first value) if (condition) else (second value)
@@ -23,6 +24,30 @@ c= int(input("enter third number"))
 min=a if a<b and a<c else b if b<c else c
 print(min)
 
+# Special operators
+#  identity operator
+#  Membership operator
+#
+# identity  operator
+#  two types of identity operator:
+#            is
+#            is not
+# to compare address   but not sano cha ki thulo cha
+# to ckeck address is same or not
 
+a=10
+b=10
+print(id(a))
+print(id(a))
+print(a is b)
+print(a is not b)
 
+ # Membership operator (in, not in)
+x= "python is fun"
+print('s' in x)
+print("s" not in x)
+
+a=["umesh", "gita", "sita"]
+print("umesh" in a)
+print("umesh" not in a)
 
